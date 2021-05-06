28093c9ce110f978a18a40dc4c1d94097838195d
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-06 10:54:34 -0700

    Create Run.cs

---
+++ scr/Run.cs
@@ -0,0 +1,10 @@
+using System;
+
+//copy and paste this code into seprate visual studio project and run to auto open the html page
+namespace Run{
+  class MainClass {
+    public static void Main (string[] args) {
+      System.Diagnostics.Process.Start("please put the file path to the main the html in your main project here");
+    }
+  }
+}
