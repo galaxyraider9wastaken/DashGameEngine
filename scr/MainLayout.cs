3ca74bc14ad7d6f3da23d76560dff1bfc48a6ad4
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-06 10:30:03 -0700

    Update and rename MainLayout to MainLayout.cs

---
+++ scr/MainLayout.cs
@@ -0,0 +1,21 @@
+using System;
+//Use Dash library
+using Dash;
+
+namespace Main{
+  class MainClass {
+  //Create DrawBatch
+  public static DrawBatch DB = new DrawBatch();
+  public static void Main (string[] args) {
+    //Set Textures here
+    var YourTexture = new Texture2D(new Vector2(YourtextureSizeX, YourtextureSizeY), "Yourtexturenamewithfileextention");
+    //Start the Game Loop 
+    while(true){
+      //Draw Textures
+      Drawing.DrawSprite(YourTexture, new Vector2(YourTextureCoordsX, YourTextureCoordsY));
+      //Draw the DrawBatch
+      DB.Draw();
+    }
+  }
+}
+}
