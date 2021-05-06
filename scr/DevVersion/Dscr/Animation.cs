e4c8bff0c046853db77593303b75d610991a19ae
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-06 12:18:59 -0700

    Update Animation.cs

--- scr/DevVersion/Dscr/Animation.cs
+++ scr/DevVersion/Dscr/Animation.cs
@@ -1,14 +1,20 @@
 using System;
+using System.Timers;
 
 namespace Dash {
   public class Animation {
     public Texture2D Frames;
     public int FrameAmount;
     public int TotalTime;
+    public Timer TimeToFrame;
+    public int CurrentFrame;
     public Animation(Texture2D[] frames, int frameamount, int totaltime //in millaseconds){
       Frames = frames;
       FrameAmount = frameamount;
       TotalTime = totaltime;
+    }
+    public void Start(){
+      TimeToFrame = new Timer(TotalTime);
     }
   }
 }
