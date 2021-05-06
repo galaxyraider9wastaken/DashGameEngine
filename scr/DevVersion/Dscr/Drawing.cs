c37f91f12921c1333bd80fa7486f635d9f3f348e
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-06 11:13:47 -0700

    Update Drawing.cs

--- scr/DevVersion/Dscr/Drawing.cs
+++ scr/DevVersion/Dscr/Drawing.cs
@@ -1,14 +1,13 @@
-using System;
-using System.IO;
-using Dash;
-
-namespace Dash : Config {
-  public class Drawing {
-    public static void DrawSprite(Texture2D Texture, Vector2 Coords){
-      using (StreamWriter writer = new StreamWriter(TDPath + "TD.js", true))
-      {  
-        writer.WriteLine("Draw("+ (int)Coords.X + ", " + (int)Coords.Y + ", " + (int)Texture.Size.X + ", " + (int)Texture.Size.Y + ", " + "\"" + Texture.File + "\"" + ");");
-      }  
+namespace Dash
+{
+    public class Drawing : Config
+    {
+        public static void DrawSprite(Texture2D Texture, Vector2 Coords)
+        {
+            using (StreamWriter writer = new StreamWriter(TDPath + "TD.js", true))
+            {
+                writer.WriteLine("Draw(" + (int)Coords.X + ", " + (int)Coords.Y + ", " + (int)Texture.Size.X + ", " + (int)Texture.Size.Y + ", " + "\"" + Texture.File + "\"" + ");");
+            }
+        }
     }
-  }
 }
